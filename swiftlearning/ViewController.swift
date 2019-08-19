80ea1b6f9bbb56fde51625eaa1b53265cfd7505f
MikeZhangpy <zhangpy19911121@gmail.com>
2019-08-19 12:51:25 +0800

    new print

--- swiftlearning/ViewController.swift
+++ swiftlearning/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("123")
+        
         // Do any additional setup after loading the view.
     }
 
