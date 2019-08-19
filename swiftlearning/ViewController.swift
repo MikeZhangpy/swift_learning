c68daf8c9023cdaa64f88a1d9bb1193fb8a615fb
zzz <zhangpy1991@126.com>
2019-08-19 14:01:27 +0800

    add print3

--- swiftlearning/ViewController.swift
+++ swiftlearning/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("123")
         print("234")
+        print("345")
         
         // Do any additional setup after loading the view.
     }
